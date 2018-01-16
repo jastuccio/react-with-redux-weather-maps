9b8a2553d1beef30b5c713ae1d41ab9d9c765027
joe <joe@astucc.io>
2018-01-16 13:18:11 -0500

    complete tutorial

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "fce04cb5049ce3a0aa3afab93aa75c83";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
