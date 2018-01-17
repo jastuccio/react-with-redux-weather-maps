6a437e5c4aa45691eaa63514e340cf01b64872e8
joe <joe@astucc.io>
2018-01-17 17:14:10 -0500

    Change search criteria from city name to zipcode

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,8 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
-export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+export function fetchWeather(zipcode) {
+  const url = `${ROOT_URL}&zip=${zipcode},us`;
   const request = axios.get(url);
 
   // console.log('Request:', request);
